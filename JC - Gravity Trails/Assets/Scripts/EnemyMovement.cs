1b397e84d8a59283f47c986b4dde877decf3d8f4
CYcneg <cy_cneg@outlook.com>
2022-09-30 17:36:11 -0700

    Gravity Trails

---
+++ JC - Gravity Trails/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float yForce;
+    public float xForce;
+    public float xDirection;
+    private Rigidbody2D enemyRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Vector2 jumpForce = new Vector2(xForce *xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+    }
+}
